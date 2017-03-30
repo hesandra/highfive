260e9a7b7e3a55cd89632a2a6827e3afb30de3d3
Sandra Jay <henkel.sandra1@gmail.com>
2017-03-30 15:43:33 -0700

    "Added signin page"

---
+++ client/src/components/Signup.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+class SignUp extends React.Component {
+  renderSignups(){
+
+  }
+}
+
+
+
+
+export default SignUp;
