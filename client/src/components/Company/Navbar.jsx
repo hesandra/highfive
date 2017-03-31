1b1b891b87cbe6d451e29f89cee8fecc14798d6f
Sandra Jay <henkel.sandra1@gmail.com>
2017-03-31 12:33:10 -0700

    "style in company navbar"

--- client/src/components/Company/Navbar.jsx
+++ client/src/components/Company/Navbar.jsx
@@ -5,50 +5,50 @@ import { Link } from 'react-router';
 const Navbar = (props) => {
   return (
     <div>
-  <div>This is the company profile</div>
-    <Tab.Container id="tabs-with-dropdown" defaultActiveKey="first">
-      <Row className="clearfix">
-        <Col sm={12}>
-          <Nav bsStyle="tabs">
-            <NavItem eventKey="first">
-            Profile
+      <div>This is the company profile</div>
+      <Tab.Container id="tabs-with-dropdown" defaultActiveKey="first">
+        <Row className="clearfix">
+          <Col sm={12}>
+            <Nav bsStyle="tabs">
+              <NavItem eventKey="first">
+               Profile
+               </NavItem>
+              <NavItem eventKey="second">
+               Submissions
             </NavItem>
-            <NavItem eventKey="second">
-            Submissions
-            </NavItem>
-            <NavDropdown eventKey="3" title="Jobposts" id="nav-dropdown-within-tab">
-              <MenuItem eventKey="3.1">Junior</MenuItem>
-              <MenuItem eventKey="3.2">Mid</MenuItem>
-              <MenuItem eventKey="3.3">Senior</MenuItem>
-              <MenuItem divider />
-              <MenuItem eventKey="3.4">Separated link</MenuItem>
-            </NavDropdown>
-          </Nav>
-        </Col>
-        <Col sm={12}>
-          <Tab.Content animation>
-            <Tab.Pane eventKey="first">
-            Company Profile
+              <NavDropdown eventKey="3" title="Jobposts" id="nav-dropdown-within-tab">
+                <MenuItem eventKey="3.1">Junior</MenuItem>
+                <MenuItem eventKey="3.2">Mid</MenuItem>
+                <MenuItem eventKey="3.3">Senior</MenuItem>
+                <MenuItem divider />
+                <MenuItem eventKey="3.4">Separated link</MenuItem>
+              </NavDropdown>
+            </Nav>
+          </Col>
+          <Col sm={12}>
+            <Tab.Content animation>
+              <Tab.Pane eventKey="first">
+              Company Profile
             </Tab.Pane>
-            <Tab.Pane eventKey="second">
-            Submissions
+              <Tab.Pane eventKey="second">
+               Submissions
             </Tab.Pane>
-            <Tab.Pane eventKey="3.1">
-            Junior
+              <Tab.Pane eventKey="3.1">
+               Junior
             </Tab.Pane>
-            <Tab.Pane eventKey="3.2">
-            Mid
+              <Tab.Pane eventKey="3.2">
+               Mid
             </Tab.Pane>
-            <Tab.Pane eventKey="3.3">
-            Senior
+              <Tab.Pane eventKey="3.3">
+               Senior
             </Tab.Pane>
-            <Tab.Pane eventKey="3.4">
+              <Tab.Pane eventKey="3.4">
             Tab 3.4 content
             </Tab.Pane>
-          </Tab.Content>
-        </Col>
-      </Row>
-    </Tab.Container>
+            </Tab.Content>
+          </Col>
+        </Row>
+      </Tab.Container>
     </div>
   );
 };
