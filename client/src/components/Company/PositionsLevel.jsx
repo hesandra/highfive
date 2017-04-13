603b58680d57d0cdc6122f272e713cfb06fdf653
andrew yi <andrew.yi.j@gmail.com>
2017-04-13 15:34:14 -0700

    date formatted

--- client/src/components/Company/PositionsLevel.jsx
+++ client/src/components/Company/PositionsLevel.jsx
@@ -15,6 +15,12 @@ class PositionsLevel extends React.Component {
       <div>
         {this.props.companyProfile.jobs.map((item, idx) => {
           if (this.props.companyProfile.level === item.level && this.props.companyProfile.companyReload[0].id === item.company_id) {
+            const cDate = new Date(item.created_at);
+            const cDateString = `${cDate.getMonth()}/${cDate.getDay()}/${cDate.getFullYear()}`;
+
+            const uDate = new Date(item.updated_at);
+            const uDateString = `${uDate.getMonth()}/${uDate.getDay()}/${uDate.getFullYear()}`;
+
             return (
                 <Table responsive>
                   <thead>
@@ -29,7 +35,7 @@ class PositionsLevel extends React.Component {
                   <tbody>
                     <tr>
                       <td>{item.id}</td>
-                      <td>{item.title}</td><td>{item.description}</td><td>{item.created_at}</td><td>{item.updated_at}</td>
+                      <td>{item.title}</td><td>{item.description}</td><td>{cDateString}</td><td>{uDateString}</td>
                       <tb><Button onClick={() => this.props.getSubmissions(item.id)}>See Submissions</Button></tb>
                       <tb><Button onClick={() => this.props.deleteJob(item.id)}>Delete</Button></tb>
                     </tr>
