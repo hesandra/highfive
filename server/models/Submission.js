a7f5bf2efd6b43c65cb4654ea177564869de2596
Jahosh <josh91hickman@gmail.com>
2017-04-14 09:44:02 -0700

    more timestamps

--- server/models/Submission.js
+++ server/models/Submission.js
@@ -1,6 +1,12 @@
 const Model = require('objection').Model;
 
 class Submission extends Model {
+  $beforeInsert() {
+    this.created_at = new Date().toISOString();
+  }
+  $beforeUpdate() {
+    this.updated_at = new Date().toISOString();
+  }
   static get tableName() {
     return 'submission';
   }
