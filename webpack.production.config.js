2f18fc2a7f30283672e59fa88e45be463de01969
Jahosh <josh91hickman@gmail.com>
2017-04-19 00:42:21 -0700

    added iso url fix, prod build

---
+++ webpack.production.config.js
@@ -0,0 +1,60 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const DotenvPlugin = require('webpack-dotenv-plugin');
+
+const SRC_DIR = path.resolve(__dirname, 'client/src');
+const PUBLIC_DIR = path.resolve(__dirname, 'client/public');
+const BUILD_DIR = path.resolve(__dirname, 'client/build');
+
+module.exports = {
+  entry: {
+    main: path.resolve(SRC_DIR, 'index.jsx'),
+  },
+  output: {
+    filename: 'bundle.js',
+    path: BUILD_DIR
+
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|js)?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        test: SRC_DIR,
+        query: {
+          presets: ['es2015', 'react', 'stage-0'],
+          plugins: ['transform-runtime', 'transform-decorators-legacy', 'transform-class-properties'],
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(jpg|png)$/,
+        use: ['file-loader'],
+      },
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin(),
+    new CopyWebpackPlugin([{ from: PUBLIC_DIR }]),
+    new DotenvPlugin({
+      sample: './.env.example',
+      path: './.env',
+    }),
+  ],
+  stats: { colors: true },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+};
