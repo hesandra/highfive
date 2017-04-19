56eaf3a300791b6f24e0b0bbab4f433384f8a946
Jahosh <josh91hickman@gmail.com>
2017-04-18 18:37:05 -0700

    server serving index

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const knexConfig = require('../knexfile');
+const path = require('path');
 
 const Model = require('objection').Model;
 const cors = require('cors');
@@ -19,16 +20,16 @@ Model.knex(knex);
 
 const app = express()
   .use(cors())
-  .use(express.static('./client'))
+  .use(express.static('../client/build'))
   .use(morgan('dev'))
   .use(bodyParser.json())
   .use(bodyParser.urlencoded({extended: true }))
   .set('json spaces', 2);
 app.options('*', cors());
 
-app.get('/*', (req, res) => {
-  res.sendFile(`${__dirname}/client/public/index.html`);
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(`${__dirname}`, 'index.html'));
+// });
 
 // pass all request to router
 app.use('/', router);
