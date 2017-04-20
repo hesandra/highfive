67b4c92d3ad065a9194a95e1cedfa36e628c6900
Jahosh <josh91hickman@gmail.com>
2017-04-19 23:39:13 -0700

    edit callback urls

--- client/src/utils/companyAuthService.js
+++ client/src/utils/companyAuthService.js
@@ -2,7 +2,7 @@ import Auth0Lock from 'auth0-lock';
 import jwtDecode from 'jwt-decode';
 
 const redirectURL = process.env.NODE_ENV === 'production' ?
-  'http://highfive-dev.us-west-2.elasticbeanstalk.com/' :
+  'http://hifivela.com/' :
   'http://localhost:8080/';
 
 export default class CompanyAuthService {
